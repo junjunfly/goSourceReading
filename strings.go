d36daf4bd43af9b581ff8a342e3135308be121b0
junjunfly <180778754@qq.com>
2017-06-28 14:54:48 +0800

    add strings.IndexFuc annotation

--- strings.go
+++ strings.go
@@ -1,4 +1,4 @@
-trings.Map(mapping,s) 源码
+//trings.Map(mapping,s) 源码
 /*
 * 此方法为了 读取s中的字符串给mapping处理，处理好了返回字符串。要注意return string(b[0:nbytes]) 一定是整个字符串长度 因为当b!=nil时，长度一定会和字** 符串s相同。 而且在替换字符串的时候都使用了ascii码.
 */
@@ -45,3 +45,21 @@ func Map(mapping func(rune) rune, s string) string {
 	}
 	return string(b[0:nbytes])//返回整个字符串
 }
+//strings.IndexFunc(s,f)源码
+//读取s中的字符串,交给f函数处理，返回第一个出现的位置eg:"abcabc" 具体还是要看函数里怎么写，如果传入函数里写 if r = 'a'{return true} 即返回a出现的第一个位置
+func indexFunc(s string, f func(rune) bool, truth bool) int {
+	start := 0
+	for start < len(s) {
+		wid := 1
+		r := rune(s[start])
+		if r >= utf8.RuneSelf {//超过单个字符的范围使用的处理方式
+			r, wid = utf8.DecodeRuneInString(s[start:])//返回ascii码和他的size
+		}
+		if f(r) == truth {//只要第一个位置出现即返回位置
+			return start
+		}
+		start += wid
+	}
+	return -1
+}
+
